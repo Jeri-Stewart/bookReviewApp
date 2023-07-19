fd23be3878f1e809e338423430b320084866474d
Jeri Stewart <jerijuba@gmail.com>
2023-07-19 13:03:10 -0400

    updated styling

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -5,15 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const bookListElement = document.getElementById("book-list");
   const reviewContainerElement = document.getElementById("review-container");
   const reviewForm = document.getElementById("review-form");
-  const updateForm = document.getElementById("update-form");
-  const selectReviewElement = document.getElementById("book-title")
-  const selectUpdateElement = document.getElementById("update-book-title")
+  const selectReviewElement = document.getElementById("book-title");
 
   const loggedInUsername = loggedInUsernameElement.innerText;
   loggedInUsernameElement.innerText = loggedInUsername;
 
   console.log("Logged-in username:", loggedInUsername);
-
 
   let books = []; // Initialize the books variable as an empty array
   let reviews = []; // Initialize the reviews variable as an empty array
@@ -48,48 +45,8 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((fetchedReviews) => {
           reviews = fetchedReviews;
 
-
           reviews.forEach((review) => {
-            const reviewCard = document.createElement("div");
-            reviewCard.classList.add("review-card");
-            reviewCard.id = `review-card-${review.reviewId}`;
-                const reviewCardImage = document.createElement("div");
-                reviewCardImage.classList.add("review-card-image");
-
-                const reviewImage = document.createElement("img");
-                reviewImage.src = review.book.image;
-                reviewImage.alt = review.book.title;
-
-                reviewCardImage.appendChild(reviewImage);
-                reviewCard.appendChild(reviewCardImage);
-
-
-            const reviewCardContent = document.createElement("div");
-            reviewCardContent.classList.add("review-card-content");
-
-            const reviewTitle = document.createElement("h3");
-            reviewTitle.classList.add("review-card-title");
-            reviewTitle.innerText = review.book.title;
-
-
-            const reviewContent = document.createElement("p");
-            reviewContent.classList.add("review-card-review");
-            reviewContent.innerText = review.review;
-
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("delete-button");
-            deleteButton.innerText = "Delete";
-            deleteButton.id = `delete-review-${review.reviewId}`;
-            deleteButton.addEventListener("click", () => {
-              deleteReview(review.reviewId);
-              reviewCard.remove();
-            });
-
-            reviewCardContent.appendChild(reviewTitle);
-            reviewCardContent.appendChild(reviewContent);
-            reviewCardContent.appendChild(deleteButton);
-            reviewCard.appendChild(reviewCardContent);
-
+            const reviewCard = createReviewCard(review);
             reviewContainerElement.appendChild(reviewCard);
           });
         })
@@ -101,81 +58,196 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Error fetching books:", error);
     });
 
-    // POST AND PUT REQUEST FOR BOOK REVIEWS
+  // Function to create a review card
+  function createReviewCard(review) {
+    const reviewCard = document.createElement("div");
+    reviewCard.classList.add("review-card");
+    reviewCard.id = `review-card-${review.reviewId}`;
 
-  reviewForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const reviewCardImage = document.createElement("div");
+    reviewCardImage.classList.add("review-card-image");
 
-    const selectedOption = selectReviewElement.options[selectReviewElement.selectedIndex];
-    const selectedOptionBookName = selectedOption.innerText;
-    const targetBook = books.find((book) => book.title === selectedOptionBookName);
-    const targetBookId = targetBook ? targetBook.bookId : null;
-    const targetBookAuthor = targetBook ? targetBook.authorId : null;
-    const targetBookImg = targetBook ? targetBook.image : null;
+    const reviewImage = document.createElement("img");
+    reviewImage.src = review.book.image;
+    reviewImage.alt = review.book.title;
 
-    const reviewInput = document.getElementById("review");
-    const reviewData = {
-      review: reviewInput.value,
-      bookId: targetBookId,
-      authorId: targetBookAuthor,
-      image: targetBookImg
-    };
+    reviewCardImage.appendChild(reviewImage);
+    reviewCard.appendChild(reviewCardImage);
 
-    const submitBtn = event.target.querySelector("button[type='submit']");
-    const buttonId = submitBtn.id;
+    const reviewCardContent = document.createElement("div");
+    reviewCardContent.classList.add("review-card-content");
 
-    if (buttonId === "create") {
-        // Check if the user has already reviewed the book
-        const alreadyReviewed = reviews.find((review) => review.bookId === targetBookId);
-        console.log(alreadyReviewed);
-        if (alreadyReviewed) {
-          alert("Book has already been reviewed. Delete or update the review.");
-          location.reload();
-          return;
-        }
+    const reviewTitle = document.createElement("h3");
+    reviewTitle.classList.add("review-card-title");
+    reviewTitle.innerText = review.book.title;
 
-        // Create review
-        fetch(`/api/v1/reviews/user/${loggedInUserId}/book/${targetBookId}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(reviewData),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("Review added:", data);
-            alert(data);
-            location.reload();
-          })
-          .catch((error) => {
-            console.error("Error adding review:", error);
-          });
-      } else if (buttonId === "updateBtn") {
-        const reviewId = parseInt(submitBtn.dataset.reviewId);
-        fetch(`/api/v1/reviews/${reviewId}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(reviewData),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("Review updated:", data);
-            const reviewCard = document.getElementById(`review-card-${reviewId}`);
-            reviewCard.querySelector(".review-card-review").innerText = reviewData.review;
-          })
-          .catch((error) => {
-            console.error("Error updating review:", error);
-          });
-      }
+    const reviewContent = document.createElement("p");
+    reviewContent.classList.add("review-card-review");
+    reviewContent.innerText = review.review;
+    reviewContent.contentEditable = false;
 
-      reviewForm.reset();
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-button");
+    editButton.innerText = "Edit";
+    editButton.addEventListener("click", () => {
+      enableEditMode(reviewContent);
     });
 
-// DELETE REQUEST FOR BOOK REVIEWS
+    const saveButton = document.createElement("button");
+    saveButton.classList.add("save-button");
+    saveButton.innerText = "Save";
+    saveButton.disabled = true;
+    saveButton.addEventListener("click", () => {
+      disableEditMode(reviewContent);
+      saveReview(review.reviewId, reviewContent.innerText);
+    });
 
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => {
+      deleteReview(review.reviewId);
+      reviewCard.remove();
+    });
+
+    reviewCardContent.appendChild(reviewTitle);
+    reviewCardContent.appendChild(reviewContent);
+    reviewCardContent.appendChild(editButton);
+    reviewCardContent.appendChild(saveButton);
+    reviewCardContent.appendChild(deleteButton);
+    reviewCard.appendChild(reviewCardContent);
+
+    return reviewCard;
+  }
+
+  // Function to enable edit mode for a review
+  function enableEditMode(reviewContent) {
+    reviewContent.contentEditable = true;
+    reviewContent.focus();
+    reviewContent.classList.add("editing");
+    reviewContent.addEventListener("input", () => {
+      enableSaveButton(reviewContent);
+    });
+  }
+
+  // Function to disable edit mode for a review
+  function disableEditMode(reviewContent) {
+    reviewContent.contentEditable = false;
+    reviewContent.classList.remove("editing");
+    disableSaveButton(reviewContent);
+  }
+
+  // Function to enable the save button for a review
+  function enableSaveButton(reviewContent) {
+    const reviewCard = reviewContent.closest(".review-card");
+    const saveButton = reviewCard.querySelector(".save-button");
+    saveButton.disabled = false;
+  }
+
+  // Function to disable the save button for a review
+  function disableSaveButton(reviewContent) {
+    const reviewCard = reviewContent.closest(".review-card");
+    const saveButton = reviewCard.querySelector(".save-button");
+    saveButton.disabled = true;
+  }
+
+// POST AND PUT REQUEST FOR BOOK REVIEWS
+
+reviewForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const selectedOption = selectReviewElement.options[selectReviewElement.selectedIndex];
+  const selectedOptionBookName = selectedOption.innerText;
+  const targetBook = books.find((book) => book.title === selectedOptionBookName);
+  const targetBookId = targetBook ? targetBook.bookId : null;
+  const targetBookAuthor = targetBook ? targetBook.authorId : null;
+  const targetBookImg = targetBook ? targetBook.image : null;
+
+  const reviewInput = document.getElementById("review");
+  const reviewData = {
+    review: reviewInput.value,
+    bookId: targetBookId,
+    authorId: targetBookAuthor,
+    image: targetBookImg
+  };
+
+  const submitBtn = event.target.querySelector("button[type='submit']");
+  const buttonId = submitBtn.id;
+
+  if (buttonId === "create") {
+    // Check if the user has already reviewed the book
+    const alreadyReviewed = reviews.find((review) => review.bookId === targetBookId);
+    console.log(alreadyReviewed);
+    if (alreadyReviewed) {
+      alert("Book has already been reviewed. Delete or update the review.");
+      location.reload();
+      return;
+    }
+
+    // Create review
+    fetch(`/api/v1/reviews/user/${loggedInUserId}/book/${targetBookId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reviewData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Review added:", data);
+        alert(data);
+        location.reload();
+      })
+      .catch((error) => {
+        console.error("Error adding review:", error);
+      });
+  } else if (buttonId === "updateBtn") {
+    const reviewId = parseInt(submitBtn.dataset.reviewId);
+    fetch(`/api/v1/reviews/${reviewId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reviewData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Review updated:", data);
+        const reviewCard = document.getElementById(`review-card-${reviewId}`);
+        reviewCard.querySelector(".review-card-review").innerText = reviewData.review;
+      })
+      .catch((error) => {
+        console.error("Error updating review:", error);
+      });
+  }
+
+  reviewForm.reset();
+});
+
+
+  // Function to save a review
+  function saveReview(reviewId, reviewText) {
+    const reviewData = {
+      review: reviewText,
+    };
+
+    fetch(`/api/v1/reviews/${reviewId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reviewData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Review updated:", data);
+      })
+      .catch((error) => {
+        console.error("Error updating review:", error);
+      });
+  }
+
+  // Function to delete a review
   function deleteReview(reviewId) {
     fetch(`/api/v1/reviews/${reviewId}`, {
       method: "DELETE",
@@ -183,14 +255,10 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.json())
       .then((data) => {
         console.log("Review deleted:", data);
-        alert(data);
-        location.reload();
       })
       .catch((error) => {
         console.error("Error deleting review:", error);
       });
   }
 });
-
-
 
